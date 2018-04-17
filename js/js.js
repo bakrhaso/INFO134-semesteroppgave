bc05d5c53bf3c9d0531b69ecd21a3e4a5e908d2e
BKS <bardsore@gmail.com>
2018-04-17 11:13:33 +0200

    endringer for å følge oblig regler

--- js/js.js
+++ js/js.js
@@ -16,7 +16,7 @@ function initMap() {
 // Laste inn JSON filen med do-dataen, sÃ¥ kaller den populateMap som legger til markers pÃ¥ kartet
 function loadJSON() {
   var request = new XMLHttpRequest();
-  request.open("GET", "../data/dokart.json", true);
+  request.open("GET", "../media/dokart.json", true);
   request.send(null)
   request.onreadystatechange = function() {
   if (request.readyState === 4 && request.status === 200) {
@@ -31,26 +31,17 @@ function loadJSON() {
 function populateTable(jsonObj) {
   var table = document.getElementById("doBord");
   var newRow, newCell;
+  var array = ["id","adresse","tid_hverdag","tid_lordag","tid_sondag","herre","dame","rullestol","pris"];
   for(var i = 1; i < jsonObj.length + 1; ++i) {
     newRow = table.insertRow(i);
-    newCell = newRow.insertCell(-1);
-    newCell.innerHTML = jsonObj[i - 1]["id"];
-    newCell = newRow.insertCell(-1);
-    newCell.innerHTML = jsonObj[i - 1]["adresse"];
-    newCell = newRow.insertCell(-1);
-    newCell.innerHTML = jsonObj[i - 1]["tid_hverdag"];
-    newCell = newRow.insertCell(-1);
-    newCell.innerHTML = jsonObj[i - 1]["tid_lordag"];
-    newCell = newRow.insertCell(-1);
-    newCell.innerHTML = jsonObj[i - 1]["tid_sondag"];
-    newCell = newRow.insertCell(-1);
-    newCell.innerHTML = jsonObj[i - 1]["herre"];
-    newCell = newRow.insertCell(-1);
-    newCell.innerHTML = jsonObj[i - 1]["dame"];
-    newCell = newRow.insertCell(-1);
-    newCell.innerHTML = jsonObj[i - 1]["rullestol"];
-    newCell = newRow.insertCell(-1);
-    newCell.innerHTML = jsonObj[i - 1]["pris"];
+    for(var j = 0; j < array.length; ++j) {
+      newCell = newRow.insertCell(-1);
+      if(jsonObj[i - 1][array[j]] == "NULL" || jsonObj[i - 1][array[j]] == "") {
+        newCell.innerHTML = "Nei";
+      } else {
+        newCell.innerHTML = jsonObj[i - 1][array[j]];
+      }
+    }
   }
 }
 
