ce9f9c2c7f20a62ecac9466d71d1fd30fef517af
BK <bardsore@gmail.com>
2018-05-01 15:10:10 +0200

    ekkel søkefunksjon

--- js/js.js
+++ js/js.js
@@ -2,6 +2,8 @@ console.log("tekst"); // Printer "tekst" i konsollen
 
 var map; // Variabel for å referere til Google kartet
 var response; // Variabel for datasett
+var markers = []; // Google Map markers
+var urlG = ""; // JSON URL
 
 // Initialiserer kartet
 function initMap() {
@@ -25,35 +27,150 @@ function loadJSON(url) {
       } else {
         response = request.response["entries"];
         populateMap(response);
-        populateList(response, url);
+        populateList(response);
       }
     }
   };
   request.send(null);
 }
 
+function search() {
+  document.getElementById("hidden").style.display = 'block';
+  document.getElementById("normalSearch").style.display = 'none';
+
+  var toalettObj = {"herre":"","dame":"","stellerom":"","rullestol":"","pris":""}
+  
+  if(document.getElementById("male").checked) {
+    toalettObj["herre"] = "1";
+  }
+  if(document.getElementById("female").checked) {
+    toalettObj["dame"] = "1";
+  }
+  if(document.getElementById("pris") != null) {
+    toalettObj["pris"] = document.getElementById("pris").value;
+  }
+  if(document.getElementById("stelle").checked) {
+    toalettObj["stellerom"] = "1";
+  }
+  if(document.getElementById("pris") == "0") {
+    toalettObj["pris"] = "0";
+  }
+  if(document.getElementById("rulle").checked) {
+    toalettObj["rullestol"] = "1";
+  }
+  /*
+  var today = new Date().getDay();
+  var time = new Date().getHours();
+  var minutt = new Date().getMinutes();
+  if(element == "åpen") {
+
+  } 
+  */
+
+  clearMarkers();
+  clearList();
+
+  var result = [];
+
+  response.forEach(e => {
+      if((toalettObj["herre"]     == e["herre"]     || toalettObj["herre"]     == "") && (toalettObj["dame"]      == e["dame"]      || toalettObj["dame"]      == "") &&
+         (toalettObj["pris"]      >= e["pris"]      || toalettObj["pris"]      == "") && (toalettObj["stellerom"] == e["stellerom"] || toalettObj["stellerom"] == "") &&
+         (toalettObj["rullestol"] == e["rullestol"] || toalettObj["rullestol"] == "")) {
+           result.push(e);
+         }
+  })
+
+  populateMap(result);
+  populateList(result);
+
+  console.log(toalettObj);
+}
+
+function quickSearch() {
+  var query = document.getElementById('searchField').value.split(" ");
+  var toalettObj = {"herre":"","dame":"","stellerom":"","rullestol":"","pris":""}
+  
+  query.forEach(element => {
+    if(element == "kjønn:mann") {
+      toalettObj["herre"] = "1";
+    }
+    if(element == "kjønn:kvinne") {
+      toalettObj["dame"] = "1";
+    }
+    if(element.substring(0,4) == "pris") {
+      toalettObj["pris"] = element.substring(5,7);
+    }
+    if(element == "stellerom") {
+      toalettObj["stellerom"] = "1";
+    }
+    if(element == "gratis") {
+      toalettObj["pris"] = "0";
+    } 
+    if(element == "rullestol") {
+      toalettObj["rullestol"] = "1";
+    }
+    /*
+    var today = new Date().getDay();
+    var time = new Date().getHours();
+    var minutt = new Date().getMinutes();
+    if(element == "åpen") {
+
+    } 
+    */
+  })
+
+  clearMarkers();
+  clearList();
+
+  var result = [];
+
+  response.forEach(e => {
+      if((toalettObj["herre"]     == e["herre"]     || toalettObj["herre"]     == "") && (toalettObj["dame"]      == e["dame"]      || toalettObj["dame"]      == "") &&
+         (toalettObj["pris"]      >= e["pris"]      || toalettObj["pris"]      == "") && (toalettObj["stellerom"] == e["stellerom"] || toalettObj["stellerom"] == "") &&
+         (toalettObj["rullestol"] == e["rullestol"] || toalettObj["rullestol"] == "")) {
+           result.push(e);
+         }
+  })
+
+  populateMap(result);
+  populateList(result);
+
+  console.log(toalettObj);
+}
+
+function clearMarkers() {
+  markers.forEach(element => {
+    element.setMap(null);
+  })
+}
+
+function clearList() {
+  document.getElementById("list").innerHTML = "";
+}
+
 // Legger til Markers på Google kartet, bruker response fra loadJSON
-function populateMap(response) {
+function populateMap(objarr) {
   var i = 1;
-  response.forEach(element => {
+  objarr.forEach(element => {
     var position = {lat: +element["latitude"], lng: +element["longitude"]};
     var marker = new google.maps.Marker({position: position, label: "" + i, map: map});
+    markers.push(marker);
     ++i;
   });
 }
 
 // Legger til navn på lekeplass/toalett i liste, bruker response fra loadJSON
-function populateList(response, url) {
+function populateList(objarr) {
   var toalettRegex = /dokart/;
   var lekeplassRegex = /lekeplass/;
   var list = document.getElementById("list");
 
-  if(toalettRegex.test(url)) {
-    response.forEach(element => {
+  if(toalettRegex.test(urlG)) {
+    objarr.forEach(element => {
       list.innerHTML += "<li>" + element["plassering"] + "</li>";
     });
-  } else if(lekeplassRegex.test(url)) {
-    response.forEach(element => {
+  } else if(lekeplassRegex.test(urlG)) {
+    objarr.forEach(element => {
       list.innerHTML += "<li>" + element["navn"] + "</li>";
     })
   }
@@ -70,17 +187,20 @@ function playVid() {
 // Laster inn kart og rett JSON url for hotspots.html
 function initHotspots() {
   initMap();
-  loadJSON("https://hotell.difi.no/api/json/bergen/dokart");
+  urlG = "https://hotell.difi.no/api/json/bergen/dokart"
+  loadJSON(urlG);
 }
 
 // Laster inn kart og rett JSON url for lekeplass.html
 function initLekeplass() {
   initMap();
-  loadJSON("https://hotell.difi.no/api/json/bergen/lekeplasser");
+  urlG = "https://hotell.difi.no/api/json/bergen/lekeplasser"
+  loadJSON(urlG);
 }
 
 // Laster JSON til dodelighet.html
 function initLekeplass() {
   initMap();
-  loadJSON("http://data.ssb.no/api/v0/dataset/102811.json");
+  urlG = "http://data.ssb.no/api/v0/dataset/102811.json"
+  loadJSON(urlG);
 }
